4c3570ccc5d112fcd773902710496e1f345d44e8
silent-pp <80565587+silent-pp@users.noreply.github.com>
2021-03-15 09:46:23 +0300

    Update LAPPO-TASK-1.py

--- Tasks/HomeWork1/LAPPO-TASK-1.py
+++ Tasks/HomeWork1/LAPPO-TASK-1.py
@@ -7,7 +7,7 @@ perc = D(input('Введите процентную ставку вклада: '
 rez = money*((D('1')+perc/(month*D('100')))**per)
 
 
-print('Вложено    : '+ str(round(money,0)), 'руб.', int((round(money,2)-int(money))*100) , 'коп.')
-print('Получено   : '+ str(int(rez)), 'руб.', int((round(rez,2)-int(rez))*100) , 'коп.')
-print('Заработано : '+ str(int(rez-money)), 'руб.', int((round(rez-money,2)-int(rez-money))*100) , 'коп.')
+print('Вложено     : '+ str(round(money,0)), 'руб.', int((round(money,2)-int(money))*100) , 'коп.')
+print('Получено    : '+ str(int(rez)), 'руб.', int((round(rez,2)-int(rez))*100) , 'коп.')
+print('Заработано  : '+ str(int(rez-money)), 'руб.', int((round(rez-money,2)-int(rez-money))*100) , 'коп.')
 
