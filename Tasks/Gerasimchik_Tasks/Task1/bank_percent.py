af0ba6e2554d019b604eb0b2086d24efc717629c
Andrew Gerasimchik <gerasimchick@tut.by>
2021-03-18 12:35:31 +0300

    Update bank_percent.py

--- Tasks/Gerasimchik_Tasks/Task1/bank_percent.py
+++ Tasks/Gerasimchik_Tasks/Task1/bank_percent.py
@@ -1,11 +1,11 @@
+from decimal import Decimal, ROUND_FLOOR
 def bank_depo(depo, rate, period):
-    rate_mont = rate / 12
-    monts = period * 12
+    rate_mont = Decimal(str(rate / 12))
+    monts = Decimal(str(period * 12))
 
-    for i in range(monts):
-        depo += round(depo * rate_mont / 100, 2)
+    s = Decimal(str(depo)) * ((1 + rate_mont/100) ** monts)
+    s = s.quantize(Decimal("1.00"), ROUND_FLOOR)
 
-    return round(depo, 2)
-
+    return (s)
 
 print(bank_depo(20000, 15, 5))
