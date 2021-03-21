b253f1bedabb9d83affd6b94d99b725eec0dceb1
zasferg <zasferg@gmail.com>
2021-03-21 18:49:46 +0300

    added input check

--- Tasks/ApekunHomeWork2/HomeTask2.py
+++ Tasks/ApekunHomeWork2/HomeTask2.py
@@ -1,123 +1,134 @@
 from datetime import datetime
 
+def choose():
 
-hours_list = ["час", "два", "три", "четыре", "пять", "шесть", "семь",
-       "восемь","девять","жесять","одинадцать","двенадцать",'тринадцать',
-       'четырнадцать','пятнадцать','шестнадцать','семнадцать','восемнадцать','девятнадцать']
-hours_list_second = ['первого', 'второго', 'третьего', 'четвертого', 'пятого', 'шестого',
-      'седьмого','восьмого','девятого','десятого','одиннадцатого','двеннадцатого']
-hours_list_third = ['час', 'часа', 'часов']
-minutes_list_a = ["десять", "двадцать", "тридцать", "двадцати",
-         "десяти"]
-minutes_list_b = ['девятнадцати', 'восемнадцати', 'семнадцати',
-         'шестнадцати','пятнадцати','четырнадцати',
-         'тринадцати','двенадцати',"одиннадцати"]
-minutes_list_c = ["одна", "двe", "три", "четыре", "пять", "шесть", "семь",
-       "восемь","девять"]
-minutes_list_d = ['девяти', 'восьми', 'семи', 'шести',
-         'пяти','четырех','трех','двух','одной']
+    choose_inp = input("choose your input. Press date or key:\n")
 
-minutes_list_e = ["одинадцать", "двенадцать"]
-minutes_list_f = ['минута', 'минуты', 'минут']
-noon_night = ['полдень', 'полуночь']
-
-
-choose_inp = input("choose your input. Press date or key:\n")
-
-if choose_inp == "date":
-    time = datetime.strftime(datetime.now(),"%H : %M")
-    time = time.split(":")
-elif choose_inp == "key":
-    time = input().split(":")
-else:
-    print("Incorrect input")
-    exit()
-
-
-my_hours = int(time[0])
-my_minutes = int(time[1])
-
-full_min = (my_minutes // 10)
-part_min = (my_minutes % 10)
-
-
-if my_hours == 12 and my_minutes == 0:
-    print(noon_night[0])
-    exit()
-elif my_hours == 24 and my_minutes == 0:
-    print(noon_night[1])
-    exit()
-
-if my_hours >= 12:
-    my_hours = my_hours - 12
-if my_hours == 1:
-    hour_word_itself = hours_list_third[0]
-elif my_hours >= 2 and my_hours <= 4:
-    hour_word_itself = hours_list_third[1]
-else:
-    hour_word_itself = hours_list_third[2]
-
-if full_min < 3:
-
-    hour_hour = hours_list[my_hours - 1]
-
-    if part_min >= 2 and part_min <= 4:
-        minutes_word_itself = minutes_list_f[1]
-    elif part_min == 1:
-        minutes_word_itself = minutes_list_f[0]
+    if choose_inp == "date":
+        time = datetime.strftime(datetime.now(),"%H : %M")
+        time = time.split(":")
+    elif choose_inp == "key":
+        time = input().split(":")
     else:
-        minutes_word_itself = minutes_list_f[2]
+        print("Incorrect input")
+        exit()
+    return time
 
-    if my_minutes > 0 and my_minutes < 10 :
-        min_s = minutes_list_c[part_min - 1]
-        print(min_s, minutes_word_itself,hours_list_second[my_hours])
+def my_clocks():
+    hours_list = ["час", "два", "три", "четыре", "пять", "шесть", "семь",
+                  "восемь", "девять", "жесять", "одинадцать", "двенадцать", 'тринадцать',
+                  'четырнадцать', 'пятнадцать', 'шестнадцать', 'семнадцать', 'восемнадцать', 'девятнадцать']
+    hours_list_second = ['первого', 'второго', 'третьего', 'четвертого', 'пятого', 'шестого',
+                         'седьмого', 'восьмого', 'девятого', 'десятого', 'одиннадцатого', 'двеннадцатого']
+    hours_list_third = ['час', 'часа', 'часов']
+    minutes_list_a = ["десять", "двадцать", "тридцать", "двадцати",
+                      "десяти"]
+    minutes_list_b = ['девятнадцати', 'восемнадцати', 'семнадцати',
+                      'шестнадцати', 'пятнадцати', 'четырнадцати',
+                      'тринадцати', 'двенадцати', "одиннадцати"]
+    minutes_list_c = ["одна", "двe", "три", "четыре", "пять", "шесть", "семь",
+                      "восемь", "девять"]
+    minutes_list_d = ['девяти', 'восьми', 'семи', 'шести',
+                      'пяти', 'четырех', 'трех', 'двух', 'одной']
 
-    elif my_minutes > 10 and my_minutes < 20:
-        slice_min_a = hours_list[10::]
-        minutes_list_b = slice_min_a[part_min - 1]
-        print(minutes_list_b, minutes_list_f[2], hours_list_second[my_hours] )
+    minutes_list_f = ['минута', 'минуты', 'минут']
+    noon_night = ['полдень', 'полуночь']
 
-    elif full_min == 0 and part_min == 0:
-        print(hour_hour, hour_word_itself)
-    elif part_min == 0:
-        minutes_list_b = minutes_list_a[full_min - 1]
-        print(minutes_list_b, minutes_word_itself,hours_list_second[my_hours] )
+    time = choose()
 
+    my_hours = int(time[0])
+    my_minutes = int(time[1])
+
+    if my_hours > 24 or my_minutes > 60 :
+
+        print("incorrect input")
+        exit()
+
+    full_min = (my_minutes // 10)
+    part_min = (my_minutes % 10)
+
+
+    if my_hours == 12 and my_minutes == 0:
+        print(noon_night[0])
+        exit()
+    elif my_hours == 24 and my_minutes == 0:
+        print(noon_night[1])
+        exit()
+
+    if my_hours >= 12:
+        my_hours = my_hours - 12
+    if my_hours == 1:
+        hour_word_itself = hours_list_third[0]
+    elif my_hours >= 2 and my_hours <= 4:
+        hour_word_itself = hours_list_third[1]
     else:
-        minutes_list_b = minutes_list_a[full_min - 1]
-        min_s = minutes_list_c[part_min - 1]
-        print(minutes_list_b, min_s, minutes_word_itself,hours_list_second[my_hours])
+        hour_word_itself = hours_list_third[2]
 
-if full_min == 3 and part_min == 0:
-    h_hour = hours_list_second[my_hours]
-    print('пол ' + h_hour)
+    if full_min < 3:
 
-if my_minutes >= 31 and my_minutes <= 59:
+        hour_hour = hours_list[my_hours - 1]
 
-    hour_hour = hours_list[my_hours]
-
-    if part_min >= 1 and part_min <= 8 or part_min == 0:
-        minutes_word_itself = minutes_list_f[2]
-    else:
-        minutes_word_itself = minutes_list_f[1]
-
-    if part_min == 0:
-        w_m = minutes_list_a[full_min - 1]
-        print('без ', w_m, minutes_word_itself, hour_hour)
-
-    if my_minutes >= 31 and my_minutes <= 39:
-        if my_hours == 12:
-            hour_hour = hours_list_third[0]
-        w_m = minutes_list_a[full_min]
-        w_mm = minutes_list_d[part_min - 1]
-        print('без ', w_m, w_mm, minutes_word_itself, hour_hour)
-
-    elif my_minutes >= 41 and my_minutes <=49:
-        w_mm = minutes_list_b[part_min - 1]
-        if part_min == 9:
+        if part_min >= 2 and part_min <= 4:
+            minutes_word_itself = minutes_list_f[1]
+        elif part_min == 1:
+            minutes_word_itself = minutes_list_f[0]
+        else:
             minutes_word_itself = minutes_list_f[2]
-        print('без ', w_mm, minutes_word_itself, hour_hour)
-    else:
-        w_mm = minutes_list_d[part_min - 1]
-        print('без ', w_mm, minutes_word_itself, hour_hour)
 
+        if my_minutes > 0 and my_minutes < 10 :
+            min_s = minutes_list_c[part_min - 1]
+            print(min_s, minutes_word_itself,hours_list_second[my_hours])
+
+        elif my_minutes > 10 and my_minutes < 20:
+            slice_min_a = hours_list[10::]
+            minutes_list_b = slice_min_a[part_min - 1]
+            print(minutes_list_b, minutes_list_f[2], hours_list_second[my_hours] )
+
+        elif full_min == 0 and part_min == 0:
+            print(hour_hour, hour_word_itself)
+        elif part_min == 0:
+            minutes_list_b = minutes_list_a[full_min - 1]
+            print(minutes_list_b, minutes_word_itself,hours_list_second[my_hours] )
+
+        else:
+            minutes_list_b = minutes_list_a[full_min - 1]
+            min_s = minutes_list_c[part_min - 1]
+            print(minutes_list_b, min_s, minutes_word_itself,hours_list_second[my_hours])
+
+    if full_min == 3 and part_min == 0:
+        h_hour = hours_list_second[my_hours]
+        print('пол ' + h_hour)
+
+    if my_minutes >= 31 and my_minutes <= 59:
+
+        hour_hour = hours_list[my_hours]
+
+        if part_min >= 1 and part_min <= 8 or part_min == 0:
+            minutes_word_itself = minutes_list_f[2]
+        else:
+            minutes_word_itself = minutes_list_f[1]
+
+        if part_min == 0:
+            w_m = minutes_list_a[full_min - 1]
+            print('без ', w_m, minutes_word_itself, hour_hour)
+
+        if my_minutes >= 31 and my_minutes <= 39:
+            if my_hours == 12:
+                hour_hour = hours_list_third[0]
+            w_m = minutes_list_a[full_min]
+            w_mm = minutes_list_d[part_min - 1]
+            print('без ', w_m, w_mm, minutes_word_itself, hour_hour)
+
+        elif my_minutes >= 41 and my_minutes <=49:
+            w_mm = minutes_list_b[part_min - 1]
+            if part_min == 9:
+                minutes_word_itself = minutes_list_f[2]
+            print('без ', w_mm, minutes_word_itself, hour_hour)
+        else:
+            w_mm = minutes_list_d[part_min - 1]
+            print('без ', w_mm, minutes_word_itself, hour_hour)
+
+
+
+
+my_clocks()
