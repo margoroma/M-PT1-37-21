f95eb8a82b4b48cfb07ebbad0729ad996fa390a8
Margot <79662931+margoroma@users.noreply.github.com>
2021-03-24 13:52:56 +0300

    Update HT2_V2
    Изменено условие ввода

--- Tasks/RM_HomeTask2+.py
+++ Tasks/RM_HomeTask2+.py
@@ -92,8 +92,6 @@ d_m = {(1,): 'минута',
 
 l_bez = [30, 40, 45, 50, 55]                          # Список исключений
 
-
-
 def time_to_word(time_var):
     t3 = time_var.replace(' ', '').split(':')
 
@@ -104,69 +102,73 @@ def time_to_word(time_var):
     m1_0 = int(m1[0])                                 # 1-я цифра в минутах
     m1_1 = int(m1[1])                                 # 2-я цифра в минутах
 
-    while True:
-
-        if int(t3[0]) >= 0 and int(t3[0]) < 25:
     
-            if int(m1) in l_bez:                              # Случаи 30-55 минут
-                polov = str(d_bez.get(int(m1)))
-                if int(m1) == 30:                             # 30 минут
-                    if h1_0 > 0:                              # Двузначное число часов
-                        h = int(h1)
-                    else:                                     # Однозначное число часов
-                        h = int(h1[1:])
-                    for key in d_pol.keys():
-                        if h in key:
-                            h_ = str(d_pol.get(key))
+    if int(m1) in l_bez:                              # Случаи 30-55 минут
+        polov = str(d_bez.get(int(m1)))
+        if int(m1) == 30:                             # 30 минут
+            if h1_0 > 0:                              # Двузначное число часов
+                h = int(h1)
+            else:                                     # Однозначное число часов
+                h = int(h1[1:])
+            for key in d_pol.keys():
+                if h in key:
+                    h_ = str(d_pol.get(key))
 
-                else:                                         # 40, 45, 50, 55 минут
-                    if h1_0 > 0 and int(h1) < 13:             # Двузначное число часов
-                        h = int(h1) + 1
-                    elif int(h1) > 12:
-                        h = int(h1) - 11
-                    else:                                     # Однозначное число часов
-                        h = int(h1[1:])+1
+        else:                                         # 40, 45, 50, 55 минут
+            if h1_0 > 0 and int(h1) < 13:             # Двузначное число часов
+                h = int(h1) + 1
+            elif int(h1) > 12:
+                h = int(h1) - 11
+            else:                                     # Однозначное число часов
+                h = int(h1[1:])+1
 
-                    dict_hour[1] = 'час'                      # Замена в словаре в случае 00 часов
-                    h_ = str(dict_hour.get(int(h)))
+            dict_hour[1] = 'час'                      # Замена в словаре в случае 00 часов
+            h_ = str(dict_hour.get(int(h)))
             
-                out = f'{polov} {h_}'
-                return out
+        out = f'{polov} {h_}'
+        return out
 
-            else: 
-                if h1_0 > 0:                                  # Двузначное число часов
-                    h = int(h1)
-                else:                                         # Однозначное число часов
-                    h = int(h1[1:])
+    else: 
+        if h1_0 > 0:                                  # Двузначное число часов
+            h = int(h1)
+        else:                                         # Однозначное число часов
+            h = int(h1[1:])
                                                 
-                for key in d_h.keys():                        # Вывод слова "часов" 
-                    if h in key:
-                        hours = d_h.get(key)
+        for key in d_h.keys():                        # Вывод слова "часов" 
+            if h in key:
+                hours = d_h.get(key)
 
-                h_ = str(dict_hour.get(int(h)))
+        h_ = str(dict_hour.get(int(h)))
 
-                if m1_1 > 0 and m1_0 in range(2,6):           # Проверка 2-х цифр в минутах
-                    m_dec = d_0.get(m1_0)
-                    m_one = dict_min.get(m1_1)
-                    m_ = f'{m_dec} {m_one}'
-                    for key in d_m.keys():                    # Вывод слова "минут"
-                        if m1_1 in key:
-                            mins = d_m.get(key)
+        if m1_1 > 0 and m1_0 in range(2,6):           # Проверка 2-х цифр в минутах
+            m_dec = d_0.get(m1_0)
+            m_one = dict_min.get(m1_1)
+            m_ = f'{m_dec} {m_one}'
+            for key in d_m.keys():                    # Вывод слова "минут"
+                if m1_1 in key:
+                    mins = d_m.get(key)
 
-                else:
-                    m_ = dict_min.get(int(m1))
-                    for key in d_m.keys():                    # Вывод слова "минут"
-                        if int(m1) in key:
-                            mins = d_m.get(key)
-                                                
-                out = f'{h_} {hours} {m_} {mins}'
-                return out
-    
         else:
-            print('Введите правильный формат времени')
-
-            break
-
+            m_ = dict_min.get(int(m1))
+            for key in d_m.keys():                    # Вывод слова "минут"
+                if int(m1) in key:
+                    mins = d_m.get(key)
+                                                
+        out = f'{h_} {hours} {m_} {mins}'
+        return out
+    
 
 print(f'Текущее время: {time_to_word(now.strftime("%H:%M"))}')
-print(f'Введенное время: {time_to_word(time_man)}')
+
+time_man = input('Введите время в формате ЧЧ:ММ, например 05:26 \n ')
+
+while True:
+    if int(time_man.replace(' ', '').split(':')[0]) >= 0 and int(time_man.replace(' ', '').split(':')[0]) < 24:
+        if int(time_man.replace(' ', '').split(':')[1]) >= 0 and int(time_man.replace(' ', '').split(':')[1]) < 60:
+            print(f'Введенное время: {time_to_word(time_man)}')
+            break
+        else:
+            time_man = input('Введите правильный формат времени (часы - от 0 до 23, минуты - от 0 до 59) \n ')
+    else:
+        time_man = input('Введите правильный формат времени (часы - от 0 до 23, минуты - от 0 до 59) \n ')
+
