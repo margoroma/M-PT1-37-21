b92e5fcfc40993290982c2139f697d2cbaf548b9
lebedev-023046 <lebedev.023046@gmail.com>
2021-03-25 13:57:41 +0300

    Sort Correct Version

--- Tasks/Lebedev_Tasks/CW/Task2C/sort.py
+++ Tasks/Lebedev_Tasks/CW/Task2C/sort.py
@@ -1,7 +1,6 @@
-arr = [8,4,6,2,1,3,5,7]
+arr = [1,3,2,4,5,7,6,8]
 for i in range(len(arr)):
     for j in range(len(arr)):
         if arr[i] < arr[j]:
             arr[i], arr[j] = arr[j], arr[i]
-        j+=1
 print(arr)
