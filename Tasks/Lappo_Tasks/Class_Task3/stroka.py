266176309193b35610829634942947bd7610c909
silent-pp <silent34@yandex.ru>
2021-03-29 23:49:06 +0300

    2 while in 1
    2 while in 1

--- Tasks/Lappo_Tasks/Class_Task3/stroka.py
+++ Tasks/Lappo_Tasks/Class_Task3/stroka.py
@@ -53,15 +53,27 @@ print("Посортировали: ",rez)
 
 
 rez2=list()
+rez3=list()
+сount=0
 i=1
 
 while i < len(sorted(rez)):
     c=rez[i-1]*rez[i]
+    
+    if c%2!=0:
+        сount+=c
+        rez3.append(c)
+
     rez2.append(c)
     i+=1
     if i >= len(sorted(rez)):
         break
     c2=rez[i-1]+rez[i]
+
+    if c2%2!=0:
+        сount+=c2
+        rez3.append(c2)
+
     rez2.append(c2)
     i+=1
     if i >= len(sorted(rez)):
@@ -69,19 +81,23 @@ while i < len(sorted(rez)):
 
 
 print("Перемножили и сложили: ",rez2)
-
-
-rez3=list()
-i=1
-сount=0
-
-while i < len(sorted(rez2)):
-    if rez2[i-1]==int(rez2[i-1]/2)*2:
-        i+=1
-        continue
-    сount+=rez2[i-1]
-    rez3.append(rez2[i-1])
-    i+=1
-    
 print("Нечетные числа: ",rez3)
 print("Сумма нечетных чисел: ",сount)
+
+
+
+# rez3=list()
+# i=1
+# сount=0
+
+# while i <= len(sorted(rez2)):
+# #    if rez2[i-1]==int(rez2[i-1]/2)*2: исправил,применил %
+#     if rez2[i-1]%2==0:
+#         i+=1
+#         continue
+#     сount+=rez2[i-1]
+#     rez3.append(rez2[i-1])
+#     i+=1
+    
+# print("Нечетные числа: ",rez3)
+# print("Сумма нечетных чисел: ",сount)
