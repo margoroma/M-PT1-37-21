664d21f40fb90cd7007c248b2735624353815928
Roman <putsilouskiroman@gmail.com>
2021-04-09 02:09:54 +0300

    no message

---
+++ Tasks/Putsilouski_Tasks/CW/6/cw6.py
@@ -0,0 +1,22 @@
+import csv
+# f = open("text_file.txt", "w")
+# f.close()
+#
+# with open("text_file.txt", "r") as f:
+#     # x = f.read(10)
+#     # print(x)
+#     # f.write("some new line\n")
+#     # f.write("another new line\n")
+#     # f.writelines(["some new line\n", "another new line"])
+#     #
+#     # for l in f:
+#     #     print(l)
+
+# with open("animals.csv", "w",newline="") as f:
+    # writer = csv.writer(f, delimiter=";")
+    # writer.writerow(["type", "alias", "age"])
+    # writer.writerow(["cat", "Tom", "70"])
+
+# with open("animals.csv", "r", newline="") as f:
+#     reader = csv.reader(f, delimiter=";")
+# ----- серилизация ------
