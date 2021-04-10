5f488aa489a50912dafe97069eb9b681e5bb78cd
margoroma <margoroma2010@gmail.com>
2021-04-10 20:38:43 +0300

    ClassWork3_CSV_JSON_Pickle_pandas

---
+++ Tasks/Ramanenka_Tasks/CW3/class_work_csv_json_pickle.py
@@ -0,0 +1,47 @@
+import csv
+import json
+import pickle
+import pandas as pd
+from IPython.display import display
+
+############################## Вывод pandas dataframe
+
+df = pd.read_csv('audi_a4_csv.csv', sep=';', error_bad_lines=False) # для просмотра данных в виде таблицы
+display(df)
+df.columns
+df.shape
+df.to_json('pandas_json.json')
+
+########################### Чтение и запись из csv в json
+
+with open('audi_a4.csv') as f:
+    reader = csv.DictReader( f, fieldnames = ( "Model","Year","Horsepower","Engine size" ) )
+    out = [ row for row in reader ]
+with open('audi_a4.json', 'w') as ff:
+    json.dump( out , ff)
+
+########################### Конвертировать из json в pd
+
+with open("pandas_json.json", 'r') as datafile:
+    data = json.load(datafile)
+
+print(data)
+print(data["Model"])
+dataframe = pd.DataFrame(data)
+print(dataframe)
+#
+# ########################### Формирование словаря из пар ключ-значение, запись в json и pickle
+with open('audi_a4.csv', "r") as f:
+    reader = csv.reader(f)
+    res = {}
+    for row in reader:
+        key = row[0]
+        res[key] = row[1:]
+    # print(type(res))
+    # print(res)
+with open("audi_a4.json", "w") as f_js:
+    json.dump(res, f_js)
+with open("audi_a4.pick", "wb") as f_pick:
+    pickle.dump(res, f_pick)
+
+print(res["80 1.9 E Quattro Specs"]) # вызов по ключу
