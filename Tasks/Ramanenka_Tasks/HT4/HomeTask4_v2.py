be026908a420f9748982a9474aec8ba7df2531c5
margoroma <margoroma2010@gmail.com>
2021-04-14 22:21:52 +0300

    HomeTask4 added

--- Tasks/Ramanenka_Tasks/HT4/HomeTask4_v2.py
+++ Tasks/Ramanenka_Tasks/HT4/HomeTask4_v2.py
@@ -1,10 +1,13 @@
 import random
 from random import randint
 
-def forming_list(n):
+k = int(input('Введите число - количество элементов списка: '))
+d = int(input('Введите максимальное число k диапозона чисел (1, k): '))
+
+def forming_list(n, max):
     s = []
     while n != 0:
-        t = randint(1, 10)
+        t = randint(1, max)
         if t not in s:
             s.append(t)
             n -= 1
@@ -19,8 +22,7 @@ def sort(list_init):
 def get_rangers(list_sorted):
     serie = []
     list_fin = []
-    for i in range(len(list_sorted)-1):
-        # print(list_sorted[i])        
+    for i in range(len(list_sorted)-1):     
         if list_sorted[i] + 1 == list_sorted[i+1]:   
             serie.append(list_sorted[i])
             serie.append(list_sorted[i+1])
@@ -38,7 +40,7 @@ def get_rangers(list_sorted):
         list_fin.append(f"{list_sorted[-1]}")
     return list_fin
 
-list0 = forming_list(7)
+list0 = forming_list(k, d)
 print(f'Список входной: {list0}')     
 
 list1 = sort(list0) 
@@ -48,5 +50,4 @@ list2 = get_rangers(list1)
 print(f'Список из строк {list2}')
 
 final = ', '.join(list2)
-print(type(final))
 print(f'Строка серий {final}')
