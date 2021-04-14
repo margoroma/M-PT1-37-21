f58f912374d63a403da38c315063989a8e9367a9
Roman <putsilouskiroman@gmail.com>
2021-04-15 01:03:17 +0300

    пробуем через рекурсию

--- Tasks/Putsilouski_Tasks/Task4/HW4.py
+++ Tasks/Putsilouski_Tasks/Task4/HW4.py
@@ -3,26 +3,40 @@ a = input("Список:\n")
 a = a.split(",")
 for i in range(len(a)):
     a[i]=a[i].replace(" ","")
+i=0
+y=len(a)
+
+def get_ranges(i,y,*args):
+    final=a[i]
+    for i in a:
+        if
+        if int(a[i])=int(a[i + 1]) - 1: continue
+        else: return 
 
 
-def get_ranges(*args):
-    final = ""
-
-    for i in range(len(a)):
-        print()
-        y = i
-        new_arg = a[i]
-        for y in range(len(a)):
-            if y == len(a) - 1:
-                break
-            if int(a[y]) == int(a[y + 1]) - 1:
-                continue
-            else:
-                new_arg = str(new_arg) + "-" + str(a[y])
-                final = final + new_arg + ","
-                i = y + 1
-                print(i)
-    print(final)
 
 
-get_ranges(*a)
+
+
+
+
+
+
+    # for i in range(len(a)):
+    #     print()
+    #     y = i
+    #     new_arg = a[i]
+    #     for y in range(len(a)):
+    #         if y == len(a) - 1:
+    #             break
+    #         if int(a[y]) == int(a[y + 1]) - 1:
+    #             continue
+    #         else:
+    #             new_arg = str(new_arg) + "-" + str(a[y])
+    #             final = final + new_arg + ","
+    #             i = y + 1
+    #             print(i)
+    # print(final)
+
+
+get_ranges(i,y,*a)
