e945374247b022d5bd37edde888917670998a378
Roman <putsilouskiroman@gmail.com>
2021-04-16 00:59:53 +0300

    Корректировка, в задании нет варианта когда в крайнем сворачивании более 2 элментов, например: 2, 3, 8, 9, 10
    До данного коммита все работало для вариантов которые в задании, но для вышеуказанного возвращало 2-3, 8-9

--- Tasks/Putsilouski_Tasks/Task4/HW4.py
+++ Tasks/Putsilouski_Tasks/Task4/HW4.py
@@ -1,81 +1,35 @@
-
-# a = input("Список:\n")
-
-a = "0, 1, 2, 3, 4, 7, 8, 10"
+a = input("Список:\n")
 a = a.split(",")
 for i in range(len(a)):
-    a[i]=a[i].replace(" ","")
-n=0
-y=len(a)
+    a[i] = a[i].replace(" ", "")
+n = 0  # начало отсчета для вызову ф
+y = len(a)
 print("Длинна спика:", y)
-z=1
 
-
-
-def get_ranges(n,y,z,*args):
-
-    print("Проход:",z)
-
+def get_ranges(n, y, *args):
     global final
-
-    if n==0:
+    if n == 0:
         final = str(a[n])
-
     else:
-        final = final+","+str(a[n])
-
+        final = final + "," + str(a[n])
     if n == y - 1:
         return (final)
-
-
     for i in range(n, y):
-
-        if i == y - 1:  #проверка на последний элемент
-            final = final + "-" + a[n+1]
+        if i == y - 1:
+            final = final + "-" + a[y - 1]
             return (final)
-
-        if int(a[i])==int(a[i + 1]) - 1: continue #проверка на последовательный
-
-        else:  #НЕ подходит
-            if i==n: #final==a[i]:
+        if int(a[i]) == int(a[i + 1]) - 1:
+            continue
+        else:
+            if i == n:
                 n = i + 1
-                z = z + 1
-                get_ranges(n, y, z, *args)
+                get_ranges(n, y, *args)
                 return (final)
             else:
-                final=final +"-"+ a[i]
+                final = final + "-" + a[i]
                 n = i + 1
-                z = z + 1
-                get_ranges(n, y, z, *args)
+                get_ranges(n, y, *args)
                 return (final)
 
 
-
-
-
-
-print(get_ranges(n, y,z, *a))
-
-
-
-
-
-
-
-
-    # for i in range(len(a)):
-    #     print()
-    #     y = i
-    #     new_arg = a[i]
-    #     for y in range(len(a)):
-    #         if y == len(a) - 1:
-    #             break
-    #         if int(a[y]) == int(a[y + 1]) - 1:
-    #             continue
-    #         else:
-    #             new_arg = str(new_arg) + "-" + str(a[y])
-    #             final = final + new_arg + ","
-    #             i = y + 1
-    #             print(i)
-    # print(final)
-
+print(get_ranges(n, y, *a))
