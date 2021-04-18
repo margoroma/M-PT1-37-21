761b17b4575616db9487c6e72505f60822a21115
margoroma <margoroma2010@gmail.com>
2021-04-18 20:22:04 +0300

    HT5 simple

---
+++ Tasks/Ramanenka_Tasks/HT5/HT5_recurs.py
@@ -0,0 +1,12 @@
+
+def sum_elem_list(lists):
+    summa = 0
+    for i in lists:
+        if type(i) != list:
+            summa += i
+        else:
+            summa += sum_elem_list(i)
+    return summa
+
+lista = [1, 2, [2, 4, [[7, 8], 4, 6]]]
+print(sum_elem_list(lista))
