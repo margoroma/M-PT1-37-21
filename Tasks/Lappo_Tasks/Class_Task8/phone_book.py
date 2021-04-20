b81d8f45b22e5d4688b6a23e924a2ffd05ed96e4
silent-pp <silent34@yandex.ru>
2021-04-21 00:22:22 +0300

    hw5

---
+++ Tasks/Lappo_Tasks/Class_Task8/phone_book.py
@@ -0,0 +1,24 @@
+book = {}
+
+
+def get_book():
+    
+def AddPhone(d):
+    fn=input("Введите имя:")
+    ln=input("Введите фамилия:")
+    ph=input("Введите телефон:")
+    if d.get((fn,ln)):
+        d[(fn,ln)].append(ph)
+    else:
+         d[(fn,ln)]=[ph]
+
+
+
+
+
+def DeletePhone(dict):
+    del dict["A1"]
+
+
+
+#def FindPhone:
