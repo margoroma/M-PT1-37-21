0cf98eaba2b3bfdc16c40a9cb8b6fc6978426aa9
Roman <putsilouskiroman@gmail.com>
2021-04-21 22:09:16 +0300

    no message

---
+++ Tasks/Putsilouski_Tasks/CW/11/cw11.py
@@ -0,0 +1,23 @@
+
+z = ["1","11","12","22","2","13","30","33"]
+
+print(sorted(filter(lambda x:int(x)*int(x)%2!=0,z)))
+
+
+
+
+
+# l = lambda x: int
+
+#
+#
+k = sorted(z, key=int)
+# filter(lambda x:int(x)*int(x)%2!=0)
+#
+# k = map(lambda k: int(k))
+#
+#
+# print(k)
+
+
+
