841bf0a417a354a5bc817c69e4814496265ae452
Andrew Gerasimchik <gerasimchick@tut.by>
2021-04-28 08:12:49 +0300

    Update arith.py

--- Tasks/Gerasimchik_Tasks/Task6/arith.py
+++ Tasks/Gerasimchik_Tasks/Task6/arith.py
@@ -1,17 +1,17 @@
 from decimal import Decimal
 
 
-def addition(a, b):
+def add(a, b):
     return Decimal(a) + Decimal(b)
 
 
-def subtraction(a, b):
+def sub(a, b):
     return Decimal(a) - Decimal(b)
 
 
-def multiplication(a, b):
+def mult(a, b):
     return Decimal(a) * Decimal(b)
 
 
-def division(a, b):
+def div(a, b):
     return Decimal(a) / Decimal(b)
