dfdf4abac6c65e5b3c8d834bfd599feee5449151
margoroma <margoroma2010@gmail.com>
2021-04-30 21:11:26 +0300

    HomeWork6_calc

--- Tasks/Ramanenka_Tasks/HT6/HT6_Calculator_with_brackets.py
+++ Tasks/Ramanenka_Tasks/HT6/HT6_Calculator_with_brackets.py
@@ -1,9 +1,6 @@
 import re
 
-# equation = input('equation: \n')
-equation = '(25 -(5- (18-3/10*4))/(-2))'
-equation = equation.replace(" ","")
-
+""" Function for checking data type"""
 def is_number(k):
     try:
         float(k)
@@ -11,8 +8,9 @@ def is_number(k):
         return False
     return True
 
+"""Function for permorming mathematical operations"""
 def operat(stroch):
-    stroch = re.findall('[+-/*//()]|-?\d+(?:\.\d+)?', stroch)
+    stroch = re.findall('[+-/*//()]|-?\d+(?:\.\d+)?', stroch) # splitting of string by characters excluding a point
     if len(stroch) == 2 and stroch[0]=="-":
         stroch = ''.join(stroch) 
         return stroch
@@ -22,12 +20,12 @@ def operat(stroch):
     else:
         if '/' in stroch:
             ind = stroch.index('/') 
-            if stroch[ind+1] != '-': 
-                reselt = float(stroch[ind-1]) / float(stroch[ind+1])
-            elif stroch[ind+1] == 0:
-                stroch = ''.join(stroch)
-                print(f'Error: division by zero {stroch}')
-                # break
+            if stroch[ind+1] != '-':
+                if float(stroch[ind+1]) == 0:
+                    print('Error: division by zero')
+                    return 0
+                else:
+                    reselt = float(stroch[ind-1]) / float(stroch[ind+1])
             else:
                 reselt = float(stroch[ind-1]) / float(stroch[ind+2]) *(-1)
                 del stroch[ind+2]
@@ -73,7 +71,7 @@ def operat(stroch):
             stroch = ''.join(stroch)
             return operat(stroch)
         return stroch
-
+"""Function for checking the negative number"""
 def check(strok_st):
     if '--' in strok_st:
         strok_st = strok_st.replace('--', '+')
@@ -81,17 +79,16 @@ def check(strok_st):
         strok_st = strok_st.replace('+-', '-')
     return strok_st    
 
+"""The first function to find a nested priority expression"""
 def recurs(st):
+    st = st.replace(" ","")
     if '(' not in st:
         return st
     else:
         a = st.rfind('(')
-        # n = st.find(')')
         n = len(st[:a])+st[a:].find(')')
         st1 = st[a+1:n]
         res = operat(st1)
         st = st.replace(st[a:n+1],str(res))
         st = check(st)
         return recurs(st)
-
-print(recurs(equation))
