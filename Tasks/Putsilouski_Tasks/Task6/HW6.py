f88655713af9c03cf52eefdcd4239cdb789d6e32
Roman <putsilouskiroman@gmail.com>
2021-05-06 03:30:45 +0300

    Complete hw 6

--- Tasks/Putsilouski_Tasks/Task6/HW6.py
+++ Tasks/Putsilouski_Tasks/Task6/HW6.py
@@ -2,19 +2,21 @@ import mtmf
 
 vyr = input("выражение\n").replace(" ","")
 
-# (4-3)*(12+(2-1))
-for y in range(len(vyr)):
-    print(vyr[y])
-    if vyr[y] == ")":
-        print("нашел")
-
-        for x in range(y, -1, -1):
-            print("зашел", vyr[x])
-            if vyr[x] == "(":
-                print("new", vyr[x])
-                pr = vyr[x:y+1]
-                print("fdsfsdfdsfsd",pr)
-                vyr = vyr.replace(vyr[x:y+1], mtmf.vn(vyr[x:y+1]))
-                break
-    else:
-        continue
+# vyr = "(4-3)*(12+(2-1))"
+while vyr.count(")"):
+    for y in range(len(vyr)):
+        if vyr[y] == ")":
+            for x in range(y, -1, -1):
+                if vyr[x] == "(":
+                    pr = []
+                    for g in range(x + 1, y):
+                        pr.append(vyr[g])
+                    pr = "".join(pr)
+                    reshenie = mtmf.vn(pr)
+                    vyr = vyr.replace(vyr[x:y + 1], str(reshenie))
+                    break
+            break
+        else:
+            continue
+itog = mtmf.vn(vyr)
+print("Результат", itog)
