e00b3c40655a9ce11d90af4351354b461fadde28
Roman <putsilouskiroman@gmail.com>
2021-05-12 02:24:27 +0300

    скорректировал предосережения

--- Tasks/Putsilouski_Tasks/Task7/HW7_implementation_not_through_a_class.py
+++ Tasks/Putsilouski_Tasks/Task7/HW7_implementation_not_through_a_class.py
@@ -2,7 +2,8 @@
 # -стена;
 # -дверь;
 # -комната;
-# Реализовать класс, который в зависимости от некоторых параметров (например, количество комнат), будет "строить" лабиринт.
+# Реализовать класс, который в зависимости от некоторых параметров (например, количество комнат),
+# будет "строить" лабиринт.
 # *Визуализировать построенный лабиринт.
 
 from random import randint
@@ -11,7 +12,7 @@ kol = int(input("Количетсво комнат:\n"))
 labirint_start = {}
 
 
-def chern_lab(labirint_start):
+def chern_lab(labirint_1):
     def dveri(komnaty, i):
         naz_k = "k" + str(i + 1)  # комната i для которой строим двери
         quantity_doors = randint(1, 2)  # количество дверей в каждой комнате от 1 до 4
@@ -30,19 +31,21 @@ def chern_lab(labirint_start):
         return komnaty
 
     for i in range(kol):
-        labirint = dveri(labirint_start, i)
+        labirint_1 = dveri(labirint_1, i)
 
-    return labirint
+    return labirint_1
+
+
 def dveri_sviaz_proverka(komnaty):
     for key in komnaty:
         door_not_update = komnaty[key]  # комната не проверенная
-        for i in range(len(door_not_update)): # для каждой двери не проверенно комнаты который называет perehod
+        for i in range(len(door_not_update)):  # для каждой двери не проверенно комнаты который называет perehod
             perehod = komnaty[door_not_update[i]]
 
             if perehod.count(key):
                 continue
             else:
-                if len(perehod) >= kol-1:
+                if len(perehod) >= kol - 1:
                     perehod.pop()
                     perehod.append(key)
 
@@ -51,15 +54,11 @@ def dveri_sviaz_proverka(komnaty):
 
                     komnaty[door_not_update[i]].append(key)
                     continue
-    return (komnaty)
-    # komnaty[door_not_update[i]]
+    return komnaty
 
 
 labirint = chern_lab(labirint_start)
 itog_lab = dveri_sviaz_proverka(labirint)
 
-
 for key in itog_lab:
     print("Из комнаты {} есть дверь в комнаты {}".format(key, itog_lab[key]))
-
-
