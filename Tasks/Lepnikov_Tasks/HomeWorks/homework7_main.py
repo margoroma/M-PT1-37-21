bf34434254d21e1e83575e736717da8112fa9296
Pavel Lepnikov <57674849+norsulfazol@users.noreply.github.com>
2021-05-12 20:07:42 +0300

    Added HomeWork7 (game v.1.0-alpha)

---
+++ Tasks/Lepnikov_Tasks/HomeWorks/homework7_main.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+###############
+# Main module #
+###############
+
+import random
+from turtle import *
+from time import sleep
+from homework7_obj import Room, PositiveHero, NegativeHero
+
+_DEFAULTS = {'room_side_len': 40,
+             'rooms_num_x': 10,
+             'rooms_num_y': 10,
+             'room_walls_num_min': 1,
+             'room_walls_num_max': 2,
+             'p_hero_shape': 2,
+             'p_hero_color': 5,
+             'p_hero_num_moves_at_time': 0,
+             'p_hero_strength': 10,
+             'n_hero_shape': 2,
+             'n_hero_color': 4,
+             'n_heroes_num_min': 1,
+             'n_heroes_num_max': 10,
+             'n_hero_num_moves_at_time': 0,
+             'n_hero_strength': 1}
+
+_FONTS = {'main': ('Arial', 18, 'bold'), 'hint': ('Arial', 10, 'bold')}
+
+if __name__ == '__main__':
+    title = '!!! BASTARDS [v.1.0-alpha] !!!'
+    window = Screen()
+    window.title(f'{title} (Initializing the maze ...)')
+    params = {'room_side_len': ('Room side length in pixels', 40, 100),
+              'rooms_num_x': ('Number of rooms horizontally', 2, 100),
+              'rooms_num_y': ('Number of rooms vertically', 2, 100),
+              'room_walls_num_min': ('Minimum number of walls in the room', 0, 3),
+              'room_walls_num_max': ('Maximum number of walls in the room', 0, 3)}
+    for k in params:
+        i = numinput('Maze parameters', f'{params[k][0]} (int, {params[k][1]}-{params[k][2]})',
+                     _DEFAULTS[k], params[k][1], params[k][2])
+        params[k] = _DEFAULTS[k] if i is None else int(i)
+    if params['room_walls_num_max'] < params['room_walls_num_min']:
+        params['room_walls_num_max'] = params['room_walls_num_min']
+    window.screensize(canvwidth=params['rooms_num_x'] * params['room_side_len'],
+                      canvheight=params['rooms_num_y'] * params['room_side_len'],
+                      bg='black')
+    window.setup(width=params['rooms_num_x'] * params['room_side_len'] + 100,
+                 height=params['rooms_num_y'] * params['room_side_len'] + 100,
+                 startx=None, starty=None)
+    title = f'{title} [{params["rooms_num_x"]}x{params["rooms_num_y"]}, {params["room_side_len"]}px]'
+    window.title(f'{title} (Building a maze ...)')
+    rooms = []
+    maze = Turtle()
+    maze.pen(shown=False,
+             pendown=False,
+             pencolor='white',
+             pensize=1,
+             speed=10)
+    maze.goto(window.screensize()[0] / -2, window.screensize()[1] / 2)
+    for y in range(params['rooms_num_y']):
+        for x in range(params['rooms_num_x']):
+            # creating a new room object
+            rooms.append(Room(side_lenght=params['room_side_len'],
+                              coords=(int(maze.xcor() + params['room_side_len'] // 2),
+                                      int(maze.ycor()) - params['room_side_len'] // 2)))
+            for i in ((1, 'right', 'left'), (params["rooms_num_x"], 'bottom', 'top')):
+                if len(rooms) > i[0] and not rooms[-i[0] - 1].side_is_wall(i[1]):
+                    rooms[-i[0] - 1].available_rooms(available_rooms={i[1]: rooms[-1]})
+                    rooms[-1].available_rooms(available_rooms={i[2]: rooms[-i[0] - 1]})
+                    rooms[-1].side_is_wall(location=i[2], is_wall=False)
+            # room walls
+            room_walls_num = random.randint(params['room_walls_num_min'], params['room_walls_num_max'])
+            # generating a list of room walls that can be removed
+            room_walls_to_remove = [i[1] for i in ((int(maze.xcor()) != int(window.screensize()[0] / 2) -
+                                                    params['room_side_len'], 'right'),
+                                                   (int(maze.ycor()) != int(window.screensize()[1] / -2) +
+                                                    params['room_side_len'], 'bottom'))
+                                    if i[0] and i[1] in rooms[-1].get_walls_locations()]
+            # adjusting the number of walls in the room
+            while len(rooms[-1].get_walls_locations()) > room_walls_num and room_walls_to_remove:
+                rooms[-1].side_is_wall(location=room_walls_to_remove.pop(random.randrange(len(room_walls_to_remove))),
+                                       is_wall=False)
+            # drawing a new room
+            for i in (('top', maze.setx, maze.xcor, 1),
+                      ('right', maze.sety, maze.ycor, -1),
+                      ('bottom', maze.setx, maze.xcor, -1),
+                      ('left', maze.sety, maze.ycor, 1)):
+                if rooms[-1].side_is_wall(location=i[0]):
+                    maze.pd()
+                else:
+                    maze.pu()
+                i[1](i[2]() + params['room_side_len'] * i[3])
+            maze.pu()
+            maze.setx(window.screensize()[0] / -2 if x == params['rooms_num_x'] - 1
+                      else maze.xcor() + params['room_side_len'])
+        maze.sety(window.screensize()[1] / 2 if y == params['rooms_num_y'] - 1
+                  else maze.ycor() - params['room_side_len'])
+    maze.goto(0, 0)
+    maze.color('blue')
+    maze.write('This is such a maze ...', align='center', font=_FONTS['main'])
+    sleep(1.5)
+    maze.undo()
+    #
+    window.title(f'{title} (Initializing and building heroes ...)')
+    heroes = []
+    # positive hero
+    hero_shapes_str = ''.join([f'\n{i} - {e}' for i, e in enumerate(PositiveHero.shapes)])
+    hero_shapes_num = len(PositiveHero.shapes)
+    hero_colors_str = ''.join([f'\n{i} - {e}' for i, e in enumerate(PositiveHero.colors)])
+    hero_colors_num = len(PositiveHero.colors)
+    params = {'p_hero_shape': (f'Positive hero shape (int, 0-{hero_shapes_num - 1}): {hero_shapes_str}',
+                               0, hero_shapes_num - 1),
+              'p_hero_color': (f'Positive hero color (int, 0-{hero_colors_num - 1}): {hero_colors_str}',
+                               0, hero_colors_num - 1),
+              'p_hero_num_moves_at_time': ('Number of positive hero moves at a time (int, 1-10, 0-random)', 0, 10),
+              'p_hero_strength': ('Positive hero strength (int, 0-100)', 0, 100)}
+    for k in params:
+        i = numinput('Positive hero parameters', params[k][0], _DEFAULTS[k], params[k][1], params[k][2])
+        params[k] = _DEFAULTS[k] if i is None else int(i)
+    if not params['p_hero_shape']:
+        params['p_hero_shape'] = random.randrange(1, hero_shapes_num)
+    if not params['p_hero_color']:
+        params['p_hero_color'] = random.randrange(1, hero_colors_num)
+    if not params['p_hero_num_moves_at_time']:
+        params['p_hero_num_moves_at_time'] = random.randint(1, 10)
+    heroes.append((PositiveHero(current_room=random.choice(rooms),
+                                num_moves_at_time=params['p_hero_num_moves_at_time'],
+                                strength=params['p_hero_strength']), Turtle()))
+    heroes[0][1].pen(shown=False,
+                     pendown=False,
+                     pencolor=PositiveHero.colors[params['p_hero_color']],
+                     pensize=1,
+                     speed=1)
+    heroes[0][1].shape(PositiveHero.shapes[params['p_hero_shape']])
+    heroes[0][1].color(PositiveHero.colors[params['p_hero_color']])
+    heroes[0][1].goto(*heroes[0][0].current_room().coords())
+    heroes[0][1].settiltangle(270)
+    heroes[0][1].st()
+    heroes[0][1].write(f'{heroes[0][0]}\n', align='center', font=_FONTS['hint'])
+    sleep(1)
+    heroes[0][1].undo()
+    # negative heroes
+    hero_shapes_str = ''.join([f'\n{i} - {e}' for i, e in enumerate(NegativeHero.shapes)])
+    hero_shapes_num = len(NegativeHero.shapes)
+    hero_colors = [i for i in NegativeHero.colors if i != PositiveHero.colors[params['p_hero_color']]]
+    hero_colors_str = ''.join([f'\n{i} - {e}' for i, e in enumerate(hero_colors)])
+    hero_colors_num = len(hero_colors)
+    params = {'n_heroes_num_min': ('Minimum number of negative heroes (int, 1-50):', 1, 50),
+              'n_heroes_num_max': ('Maximum number of negative heroes (int, 1-50):', 1, 50),
+              'n_hero_shape': (f'Negative hero shape (int, 0-{hero_shapes_num - 1}): {hero_shapes_str}',
+                               0, hero_shapes_num - 1),
+              'n_hero_color': (f'Negative hero color (int, 0-{hero_colors_num - 1}): {hero_colors_str}',
+                               0, hero_colors_num - 1),
+              'n_hero_num_moves_at_time': ('Number of negative hero moves at a time (int, 1-10, 0-random)', 0, 10),
+              'n_hero_strength': ('Negative hero strength (int, 0-100)', 0, 100)}
+    for k in params:
+        i = numinput('Negative heroes parameters', params[k][0], _DEFAULTS[k], params[k][1], params[k][2])
+        params[k] = _DEFAULTS[k] if i is None else int(i)
+    if params['n_heroes_num_max'] < params['n_heroes_num_min']:
+        params['n_heroes_num_max'] = params['n_heroes_num_min']
+    for _ in range(random.randint(params['n_heroes_num_min'], params['n_heroes_num_max'])):
+        hero_shape = NegativeHero.shapes[
+            params['n_hero_shape'] if params['n_hero_shape'] else random.randrange(1, hero_shapes_num)]
+        hero_color = hero_colors[
+            params['n_hero_color'] if params['n_hero_color'] else random.randrange(1, hero_colors_num)]
+        hero_num_moves_at_time = params['n_hero_num_moves_at_time'] if params[
+            'n_hero_num_moves_at_time'] else random.randint(1, 10)
+        heroes.append((NegativeHero(current_room=random.choice(rooms),
+                                    num_moves_at_time=hero_num_moves_at_time,
+                                    strength=params['n_hero_strength']), Turtle()))
+        while heroes[-1][0].current_room() is heroes[0][0].current_room():  # ! inf if n_heroes_num => len(rooms)-1
+            heroes[-1][0].current_room(random.choice(rooms))
+        heroes[-1][1].pen(shown=False,
+                          pendown=False,
+                          pencolor=hero_color,
+                          pensize=1,
+                          speed=1)
+        heroes[-1][1].shape(hero_shape)
+        heroes[-1][1].color(hero_color)
+        heroes[-1][1].goto(*heroes[-1][0].current_room().coords())
+        heroes[-1][1].settiltangle(90)
+        heroes[-1][1].st()
+        heroes[-1][1].write(f'{heroes[-1][0]}\n', align='center', font=_FONTS['hint'])
+        sleep(1)
+        heroes[-1][1].undo()
+    # game loop
+    maze.write("Let's start the battle ...", align='center', font=_FONTS['main'])
+    sleep(1.5)
+    maze.undo()
+    window.title(f'{title} (Battle ...)')
+    while heroes[0][0].strength() >= 0 and heroes[1:]:
+        # heroes movements
+        for h in range(2):
+            if heroes[0][0].strength() < 0 or not heroes[1:]:
+                break
+            hero = (heroes[0], random.choice(heroes[1:]))[h]
+            for _ in range(hero[0].num_moves_at_time()):
+                available_rooms = list(hero[0].current_room().available_rooms().values())
+                if available_rooms:
+                    random_room = random.choice(available_rooms)
+                    hero[1].settiltangle((0, 90, 180, 270)[('right', 'top', 'left', 'bottom').index(
+                        hero[0].current_room().get_available_room_location(random_room))])
+                    hero[1].goto(*random_room.coords())
+                    hero[0].current_room(random_room)
+                    contact_hero = None
+                    for i in heroes[1:]:
+                        if not i[1].distance(heroes[0][1].xcor(), heroes[0][1].ycor()):
+                            contact_hero = i
+                            break
+                    if contact_hero:
+                        heroes[0][1].resizemode('user')
+                        contact_hero[1].resizemode('user')
+                        for i in range(3):
+                            for j in (contact_hero[1], heroes[0][1]):
+                                j.shapesize(2, 2)
+                                sleep(.2)
+                                j.undo()
+                                sleep(.2)
+                        heroes[0][0].strength(heroes[0][0].strength() - contact_hero[0].strength())
+                        heroes[0][1].write(f'{"losing" if heroes[0][0].strength() < 0 else "winning"}\n',
+                                           align='center', font=_FONTS['hint'])
+                        sleep(1)
+                        heroes[0][1].undo()
+                        if heroes[0][0].strength() < 0:
+                            heroes[0][1].ht()
+                            break
+                        contact_hero[1].ht()
+                        heroes.remove(contact_hero)
+    maze.write(f'Positive hero {"lost" if heroes[0][0].strength() < 0 else "won"}!', align='center',
+               font=_FONTS['main'])
+    sleep(1.5)
+    maze.undo()
+    maze.write('GAME OVER', align='center', font=_FONTS['main'])
+    sleep(1.5)
+    maze.undo()
+    window.mainloop()
---
+++ Tasks/Lepnikov_Tasks/HomeWorks/homework7_obj.py
@@ -0,0 +1,121 @@
+#################
+# Object module #
+#################
+
+import abc
+
+
+class RoomSide:
+    locations = ('left', 'right', 'top', 'bottom')
+
+    def __init__(self, **kwds):
+        self.__location = self.locations[0]
+        self._location(kwds.get('location'))
+        self.__lenght = 40
+        self._lenght(kwds.get('lenght'))
+        self.__is_wall = True
+        self._is_wall(kwds.get('is_wall'))
+
+    def _location(self, location=None):
+        if isinstance(location, str) and location in self.locations:
+            self.__location = location
+        return self.__location
+
+    def _lenght(self, lenght=None):
+        if isinstance(lenght, int):
+            self.__lenght = lenght
+        return self.__lenght
+
+    def _is_wall(self, is_wall=None):
+        if is_wall is not None:
+            self.__is_wall = bool(is_wall)
+        return self.__is_wall
+
+
+class Room:
+    def __init__(self, **kwds):
+        self.__sides = {i: RoomSide(location=i, lenght=kwds.get('side_lenght')) for i in RoomSide.locations}
+        self.__coords = (0, 0)
+        self.coords(kwds.get('coords'))
+        self.__available_rooms = {}
+        self.available_rooms(available_rooms=kwds.get('available_rooms'), update=False)
+
+    def __str__(self):
+        return f'Room at {self.__coords}'
+
+    def coords(self, coords=None):
+        if isinstance(coords, (list, tuple)):
+            self.__coords = tuple(coords)
+        return self.__coords
+
+    def side_lenght(self, lenght=None):
+        if isinstance(lenght, int):
+            for v in self.__sides.values():
+                v._lenght(lenght)
+        return self.__sides[list(self.__sides.keys())[0]]._lenght()
+
+    def side_is_wall(self, location=None, is_wall=None):
+        if isinstance(location, str) and location in self.__sides:
+            if is_wall is not None:
+                self.__sides[location]._is_wall(is_wall)
+            return self.__sides[location]._is_wall()
+        # ! return None = False
+
+    def get_walls_locations(self):
+        return [k for k, v in self.__sides.items() if v._is_wall()]
+
+    def available_rooms(self, available_rooms=None, update=True):
+        if isinstance(available_rooms, dict):
+            if update:
+                self.__available_rooms.update(available_rooms)
+            else:
+                self.__available_rooms = available_rooms
+        return self.__available_rooms
+
+    def get_available_room_location(self, available_room=None):
+        if isinstance(available_room, Room):
+            if available_room in self.__available_rooms.values() and self in available_room.available_rooms().values():
+                return list(self.__available_rooms)[list(self.__available_rooms.values()).index(available_room)]
+        # ! return None
+
+
+class Hero(abc.ABC):
+    shapes = ('random', 'classic', 'turtle', 'arrow', 'circle', 'square', 'triangle')
+    colors = ('random', 'white', 'yellow', 'orange', 'red', 'blue', 'green', 'purple')
+
+    def __init__(self, **kwds):
+        self.__current_room = None
+        self.current_room(kwds.get('current_room'))
+        self.__num_moves_at_time = 1
+        self.num_moves_at_time(kwds.get('num_moves_at_time'))
+        self.__strength = 0
+        self.strength(kwds.get('strength'))
+
+    @abc.abstractmethod
+    def __str__(self):
+        pass
+
+    def current_room(self, current_room=None):
+        if isinstance(current_room, Room):
+            self.__current_room = current_room
+        return self.__current_room
+
+    def num_moves_at_time(self, num_moves_at_time=None):
+        if isinstance(num_moves_at_time, int):
+            self.__num_moves_at_time = num_moves_at_time
+        return self.__num_moves_at_time
+
+    def strength(self, strength=None):
+        if isinstance(strength, int):
+            self.__strength = strength
+        return self.__strength
+
+
+class PositiveHero(Hero):
+    def __str__(self):
+        return f'Positive hero in {self.current_room()}'.capitalize()
+
+
+class NegativeHero(Hero):
+    def __str__(self):
+        return f'Negative hero in {self.current_room()}'.capitalize()
